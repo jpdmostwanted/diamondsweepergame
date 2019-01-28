9ba1d4a42ed0400f2f8e91b335b1d9c52019593e
jyotirmaya prusty <32849868+jpdmostwanted@users.noreply.github.com>
2019-01-29 00:40:22 +0530

    hint added with dynamic slope
    added hint

--- public/assets/app.js
+++ public/assets/app.js
@@ -73,68 +73,84 @@
 var cellNum = 1;
 var cell = {};
 var diamonds = {};
-var winCount = 0;
 global.startApp = function (container) {
-    console.log("Here is the container:", container);
-    $(document).ready(function () {
-        generateRandom();
-        initGame();
+  console.log("Here is the container:", container);
+  $(document).ready(function () {
+    generateRandom();
+    initGame();
 
-        $("#start-new-game").on("click", function () {
-            generateRandom();
-            winCount = 0;
-            $('td').css('transform', 'none').removeClass('arrow diamond disabled').addClass('unknown');
-        });
+    $("#restart").on("click", function () {
+      generateRandom();
+      initGame();
     });
+  });
 };
 
 function generateRandom() {
-    while (Object.keys(diamonds).length < 8) {
-        var randomnumber = Math.floor(Math.random() * 64) + 1;
-        diamonds[randomnumber] = randomnumber;
-    }
+  while (Object.keys(diamonds).length < 8) {
+    var randomnumber = Math.floor(Math.random() * 64) + 1;
+    diamonds[randomnumber] = randomnumber;
+  }
 }
 
 function initGame() {
-    var table = document.createElement("table");
-    table.id = "board";
-    table.className = "table-bordered";
-    for (var i = 1; i <= 8; i++) {
-        var tr = document.createElement('tr');
-        for (var j = 1; j <= 8; j++) {
-            var td = document.createElement('td');
-            cell[cellNum] = {
-                x: i,
-                y: j
-            };
-            td.className = "cell unknown";
-            td.id = cellNum;
-            cellNum++;
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
+  var table = document.createElement("table");
+  table.id = "board";
+  table.className = "table-bordered";
+  for (var i = 1; i <= 8; i++) {
+    var tr = document.createElement('tr');
+    for (var j = 1; j <= 8; j++) {
+      var td = document.createElement('td');
+      cell[cellNum] = {
+        x: i,
+        y: j
+      };
+      td.className = "cell unknown";
+      td.id = cellNum;
+      cellNum++;
+      tr.appendChild(td);
     }
-    document.getElementById("gameboard").innerHTML = "";
-    document.getElementById("gameboard").appendChild(table);
-    $(".cell").on("click", clickHandler);
+    table.appendChild(tr);
+  }
+  document.getElementById("gameboard").innerHTML = "";
+  document.getElementById("gameboard").appendChild(table);
+  $(".cell").on("click", clickHandler);
 }
 
 function clickHandler(e) {
+  if (!e.target.classList.contains('disabled')) {
     if (Object.keys(diamonds).length) {
-        //if (e.target.nodeName == 'TD') {
-        winCount++;
-        if (diamonds[e.target.id]) {
-            e.target.className = "cell diamond disabled";
-            delete diamonds[e.target.id];
-            if (Object.keys(diamonds).length == 0) {
-                $('#winner').modal('show');
-                $('#winScore').empty().text(64 - winCount);
-            }
-        } else {
-            e.target.className = 'cell';
+      if (diamonds[e.target.id]) {
+        $('.arrow').removeClass('arrow');
+        e.target.className = "cell diamond disabled";
+        delete diamonds[e.target.id];
+        if (Object.keys(diamonds).length == 0) {
+          $('#winner').modal('show');
+          $('#winScore').empty().text($('.unknown').length);
         }
-        //}
+      } else {
+        var rotate = hint(e.target.id);
+        $('.arrow').removeClass('arrow');
+        e.target.className = "cell arrow disabled";
+        e.target.style["transform"] = "rotate(" + rotate + "deg)";
+      }
     }
+  }
+}
+
+function minDistance(clicked_id) {
+  var distanceMap = {};
+  Object.keys(diamonds).map(function (id) {
+    distanceMap[id] = Math.abs(cell[clicked_id].x - cell[id].x) + Math.abs(cell[clicked_id].y - cell[id].y);
+  });
+  return Object.keys(distanceMap).sort(function (a, b) {
+    return distanceMap[a] - distanceMap[b];
+  })[0];
+}
+
+function hint(clicked_id) {
+  var nearestDiamondId = minDistance(clicked_id);
+  return Math.atan2(cell[nearestDiamondId].x - cell[clicked_id].x, cell[nearestDiamondId].y - cell[clicked_id].y) * 180 / Math.PI;
 }
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
 
